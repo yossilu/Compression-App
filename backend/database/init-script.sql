428bde709d16c7d4a932c10dccdee2941290548e
yossilu <34454018+yossilu@users.noreply.github.com>
2021-08-29 08:47:26 +0300

    Update init-script.sql

--- backend/database/init-script.sql
+++ backend/database/init-script.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE party_transactions(
+CREATE TABLE IF NOT EXISTS party_transactions(
     traidingParty VARCHAR(255) NOT NULL,
     counterparty VARCHAR(255) NOT NULL,
     amount INTEGER
